2ec812d9dc45af0ec6765afd1705eccf7ae22406
VladWhite22 <vladislav22971@mail.ru>
2024-12-07 03:53:40 +0300

    KTB-02-branch-name Исправленный

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,7 @@ package org.example
 import java.io.File
 
 fun main() {
-   val wordsFile:File = File("words.txt")
-    wordsFile.createNewFile()
+    val wordsFile: File = File("words.txt")
 
-    for (words in wordsFile.readLines())
-        println(words)
+    wordsFile.forEachLine { println(it) }
 }

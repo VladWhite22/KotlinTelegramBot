04554dd400d3d5ca653a9af7b55b654a4b264051
VladWhite22 <vladislav22971@mail.ru>
2024-12-11 02:53:32 +0300

    KTB-04 Спарсить файл словаря и распечатать содержимое Исправленный

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,13 +4,13 @@ import java.io.File
 
 fun main() {
     val wordsFile: File = File("words.txt")
+    val dictionary = mutableListOf<Word>()
 
     wordsFile.forEachLine {
-        val dictionary = mutableListOf<Word>()
-
-        val line = it.split(" ","|")
-        val word = Word(original = line[0], translete = line[1], correctAnswersCount = line[2].toInt()?:0)
+        val line = it.split("|")
+        val word = Word(original = line[0], translete = line[1], correctAnswersCount = line.getOrNull(2)?.toIntOrNull() ?: 0)
         dictionary.add(word)
-        for (element in dictionary) println("${element.original} ${element.translete} ${element.correctAnswersCount}")
     }
+
+    dictionary.forEach { println("${it.original} ${it.translete} ${it.correctAnswersCount}") }
 }

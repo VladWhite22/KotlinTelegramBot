27fcc92e5bd93b986d6de08df92105306a1b0cd2
VladWhite22 <vladislav22971@mail.ru>
2024-12-14 02:24:19 +0300

    KTB-06 Реализовать функционал статистики

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,42 @@ package org.example
 import java.io.File
 
 fun main() {
-    val wordsFile: File = File("words.txt")
-    val dictionary = mutableListOf<Word>()
+    val dictionary = loadDictionary()
+    while (true) {
+        println(
+            "Меню: \n" +
+                    "1 – Учить слова\n" +
+                    "2 – Статистика\n" +
+                    "0 – Выход"
+        )
 
+        val userChoice = readln().toInt()
+
+        when (true) {
+            (userChoice == 1) -> println("Вы выбрали: Учить слова")
+            (userChoice == 2) -> {
+                println("Вы выбрали: Статистика")
+                val learnedCount = dictionary.filter { it.correctAnswersCount >= 3 }.size
+                val totalCount = dictionary.size
+                val percent = (learnedCount.toFloat() / totalCount.toFloat() * 100).toInt()
+                println("Выучено $learnedCount из $totalCount слов | $percent%" + "\n")
+            }
+
+            (userChoice == 0) -> break
+            else -> println("Введите число 1, 2 или 0")
+        }
+    }
+}
+
+fun loadDictionary(): List<Word> {
+    val dictionary = mutableListOf<Word>()
+    val wordsFile: File = File("words.txt")
     wordsFile.forEachLine {
         val line = it.split("|")
-        val word = Word(original = line[0], translete = line[1], correctAnswersCount = line.getOrNull(2)?.toIntOrNull() ?: 0)
+        val word =
+            Word(original = line[0], translete = line[1], correctAnswersCount = line.getOrNull(2)?.toIntOrNull() ?: 0)
         dictionary.add(word)
     }
 
-    dictionary.forEach { println("${it.original} ${it.translete} ${it.correctAnswersCount}") }
+    return dictionary
 }

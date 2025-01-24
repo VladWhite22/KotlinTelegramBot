50cbfa50852b71f5cad4c3a535ed9565fc0a17fb
VladWhite22 <vladislav22971@mail.ru>
2025-01-24 20:14:13 +0300

    KTB-14 Распарсить строку, используя регулярные выражения (RegEx)

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -12,19 +12,30 @@ fun main(args: Array<String>) {
 
     while (true) {
         Thread.sleep(2000)
-        val updates = getUpdates(botToken, updateId)
+        val updates = getUpdates(botToken, updateId.toString())
         println(updates)
 
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
-        println(updates)
-        updateId = updateIdString.toInt() + 1
+        val updateIdRegex = "\"update_id\":(.+?),".toRegex()
+        val matchResultId: MatchResult? = updateIdRegex.find(updates)
+        val groupsId = matchResultId?.groups
+        val id = groupsId?.get(1)?.value?.toInt()
+
+        if (id != null) {
+            updateId = id
+        }
+        println(updateId)
+
+
+        val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+        val matchResult: MatchResult? = messageTextRegex.find(updates)
+        val groups = matchResult?.groups
+        val text = groups?.get(1)?.value
+        println(text)
+
     }
 }
 
-fun getUpdates(botToken: String, updateId: Int): String {
+fun getUpdates(botToken: String, updateId: String?): String {
     val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
 
     val client: HttpClient = HttpClient.newBuilder().build()

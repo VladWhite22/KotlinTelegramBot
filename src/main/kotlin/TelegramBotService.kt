4e8655fe0c1d2d5c1f286b51d3c2b1e717be8391
VladWhite22 <vladislav22971@mail.ru>
2025-01-28 01:27:14 +0300

    KTB-15 Отправить сообщение пользователю через Telegram API Исправленный

--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -6,10 +6,11 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 class TelegramBotService(private val botToken: String) {
-    fun getUpdates(botToken: String, updateId: String?): String {
-        val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
+    private val client: HttpClient = HttpClient.newBuilder().build()
 
-        val client: HttpClient = HttpClient.newBuilder().build()
+    fun getUpdates(botToken: String, updateId: String?): String {
+        val urlGetUpdates = "$API_TELEGRAM$botToken/getUpdates?offset=$updateId"
+
         val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
         val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
 
@@ -19,9 +20,8 @@ class TelegramBotService(private val botToken: String) {
     fun sendMessage(chat_id: String, text: String) {
         if (text.length > 4096 || text.length < 1) return println("Недопустимое значение text")
 
-        val urlSendMessage = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chat_id&text=$text"
+        val urlSendMessage = "$API_TELEGRAM$botToken/sendMessage?chat_id=$chat_id&text=$text"
 
-        val client: HttpClient = HttpClient.newBuilder().build()
         val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
         client.send(request, HttpResponse.BodyHandlers.ofString())
     }
